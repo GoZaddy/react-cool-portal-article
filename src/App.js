b6d97fb11db4ac00e1830be5e48519ec2614a331
Yusuff Faruq <faruqyusuff437@gmail.com>
2020-04-21 17:41:38 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+import usePortal from "react-cool-portal";
 
 function App() {
+  const { Portal, show, hide } = usePortal({
+    defaultShow: false,
+    onShow: () => {
+      setAnimationState("slideIn");
+    },
+  });
+
+  const [showModal, setShowModal] = useState(false);
+  const [animationState, setAnimationState] = useState("");
+  console.log(animationState);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button
+        onClick={() => {
+          setShowModal(true);
+          show();
+          
+        }}
+      >
+        Open Modal
+      </button>
+      {showModal && (
+        <Portal>
+          <div className= "modal" tabIndex={-1}>
+            <div
+              className={`modal-dialog ${animationState}`}
+              role="dialog"
+              aria-labelledby="modal-header"
+              aria-modal="true"
+              onAnimationEnd={() => {
+                if(animationState == "slideOut"){
+                  hide();
+                }
+                setAnimationState("");
+              }}
+            >
+              <div className="modal-header">
+                <h5 id="modal-label">Modal header</h5>
+                <span
+                  className="modal-exit"
+                  onClick={() => {
+                    setAnimationState("slideOut");
+                  }}
+                >
+                  close
+                </span>
+              </div>
+              <div className="modal-body">
+                <p>Modal Body</p>
+              </div>
+            </div>
+          </div>
+        </Portal>
+      )}
     </div>
   );
 }
